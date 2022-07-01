0061c55cf306961b35727c3d39c532dcafbf53f8
Nisarg Patel <npate112@uottawa.ca>
2022-07-01 08:58:54 -0400

    Updates

---
+++ precache-manifest.2c34e477975f42444fd70ce44fbdc2a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c6ee7b1c65ef68c5f9dc1a01c1ba6ae",
+    "url": "/SEG3125_RecepieApp_WithHosting/index.html"
+  },
+  {
+    "revision": "b9832f17eb545799a56a",
+    "url": "/SEG3125_RecepieApp_WithHosting/static/css/main.f947e3fd.chunk.css"
+  },
+  {
+    "revision": "ffa0979bba4591a94c78",
+    "url": "/SEG3125_RecepieApp_WithHosting/static/js/2.22a4cc2b.chunk.js"
+  },
+  {
+    "revision": "7f3c8c8427e1c9d720d63db262637511",
+    "url": "/SEG3125_RecepieApp_WithHosting/static/js/2.22a4cc2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9832f17eb545799a56a",
+    "url": "/SEG3125_RecepieApp_WithHosting/static/js/main.0cdfe328.chunk.js"
+  },
+  {
+    "revision": "6f6d0a35510a25c7b34f",
+    "url": "/SEG3125_RecepieApp_WithHosting/static/js/runtime-main.3baa5aa9.js"
+  },
+  {
+    "revision": "1185e868dd5d467843a9fc7936569ef5",
+    "url": "/SEG3125_RecepieApp_WithHosting/static/media/logo.1185e868.svg"
+  }
+]);
